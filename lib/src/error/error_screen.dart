90c5199fc00bb1cde5a337edde84058656929c49
smzn1222 <satihase1123@gmail.com>
2024-08-16 17:01:32 +0900

    エラーページにホームボタン追加

--- lib/src/error/error_screen.dart
+++ lib/src/error/error_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ErrorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Error! Please restart the app.'),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text('Error! Please restart the app.'),
+        ElevatedButton(
+          onPressed: () {
+            GoRouter.of(context).go('/');
+          },
+          child: Text('HOME'),
+        ),
+      ]),
     );
   }
 }
